2ce1b25fba2fff52f830b243c1528973cbf5ad65
Aazib Chaudhry <aazibch47@gmail.com>
2023-03-15 11:45:41 +0500

    Edit page content

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -87,8 +87,8 @@ const CreatePost = () => {
       <div>
         <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
         <p className="mt-2 text-[#666e75] text-[16px] max-w-[500px]">
-          Create imaginative and visually appealing images through the Stable
-          Diffusion model and share them with the community.
+          Create imaginative and visually appealing images with Stable Diffusion
+          and share them with the community.
         </p>
       </div>
       <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
